035e8547a2abf20d29182c0230cd2745e8c3f6d0
Olav Frengstad <olav@fwt.no>
2014-08-27 10:55:54 +0200

    Remove unecassery comments

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,7 +1,3 @@
-# curl -XPUT http://localhost:8098/search/index/user
-# bin/riak-admin bucket-type create user '{"props":{"search_index":"user"}}'     
-# bin/riak-admin bucket-type activate user
-
 ExUnit.start
 
 Code.require_file "utils.exs", __DIR__
